c09f7bd41f5f3487ba5894944c678803db8e92ff
Woo Jia Hao <woojiahao1234@gmail.com>
2023-08-07 15:13:06 +0800

    Setup Tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // mode: 'jit',
+  // purge: [
+  //   './public/**/*.html',
+  //   './src/**/*.{js,jsx,ts,tsx}',
+  // ],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
